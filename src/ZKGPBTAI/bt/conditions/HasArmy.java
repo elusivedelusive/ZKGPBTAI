0e4631bc50b65f598ade55cde63c36793422d516
Jonatan Hoff <jona341@hotmail.com>
2016-02-04 11:17:03 +0100

    finalizing for merge

--- src/ZKGPBTAI/bt/conditions/HasArmy.java
+++ src/ZKGPBTAI/bt/conditions/HasArmy.java
@@ -7,6 +7,7 @@ import bt.leaf.Condition;
  * Created by Jonatan on 26-Jan-16.
  */
 public class HasArmy extends Condition<Main>{
+
     @Override
     protected boolean condition() {
         //Main.INSTANCE.getCallback().getGame().sendTextMessage("HasArmy", 0);
