fdcc637c20bb0492851e0fc270591cd6b645065b
Jonatan Hoff <jona341@hotmail.com>
2016-02-15 11:40:42 +0100

    added bt nodes for managing workers

---
+++ src/ZKGPBTAI/bt/actions/worker/AssistFactory.java
@@ -0,0 +1,25 @@
+package ZKGPBTAI.bt.actions.worker;
+
+import ZKGPBTAI.Main;
+import bt.Task;
+import bt.leaf.Action;
+import ec.EvolutionState;
+import ec.Problem;
+import ec.gp.ADFStack;
+import ec.gp.GPData;
+import ec.gp.GPIndividual;
+
+/**
+ * Created by Jonatan on 15-Feb-16.
+ */
+public class AssistFactory extends Action<Main> {
+    @Override
+    public Task.TaskState execute() {
+        return null;
+    }
+
+    @Override
+    public void eval(EvolutionState evolutionState, int i, GPData gpData, ADFStack adfStack, GPIndividual gpIndividual, Problem problem) {
+
+    }
+}
---
+++ src/ZKGPBTAI/bt/actions/worker/BuildGauss.java
@@ -0,0 +1,24 @@
+package ZKGPBTAI.bt.actions.worker;
+
+import ZKGPBTAI.Main;
+import bt.leaf.Action;
+import ec.EvolutionState;
+import ec.Problem;
+import ec.gp.ADFStack;
+import ec.gp.GPData;
+import ec.gp.GPIndividual;
+
+/**
+ * Created by Jonatan on 15-Feb-16.
+ */
+public class BuildGauss extends Action<Main> {
+    @Override
+    public TaskState execute() {
+        return null;
+    }
+
+    @Override
+    public void eval(EvolutionState evolutionState, int i, GPData gpData, ADFStack adfStack, GPIndividual gpIndividual, Problem problem) {
+
+    }
+}
