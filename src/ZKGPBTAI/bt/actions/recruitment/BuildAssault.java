62b6b7aa7e9bda5275ba588566655f18d2cc62fb
Jonatan Hoff <jona341@hotmail.com>
2016-02-15 14:19:18 +0100

    Added recruitment bt nodes

---
+++ src/ZKGPBTAI/bt/actions/recruitment/BuildAssault.java
@@ -0,0 +1,25 @@
+package ZKGPBTAI.bt.actions.recruitment;
+
+import ZKGPBTAI.Main;
+import bt.Task;
+import bt.leaf.Action;
+import ec.EvolutionState;
+import ec.Problem;
+import ec.gp.ADFStack;
+import ec.gp.GPData;
+import ec.gp.GPIndividual;
+
+/**
+ * Created by Jonatan on 15-Feb-16.
+ */
+public class BuildAssault extends Action<Main> {
+    @Override
+    public TaskState execute() {
+        return null;
+    }
+
+    @Override
+    public void eval(EvolutionState evolutionState, int i, GPData gpData, ADFStack adfStack, GPIndividual gpIndividual, Problem problem) {
+
+    }
+}
