3e4cb3d6d8720349baa644ee9086464d5bb0a511
Jonatan Hoff <jona341@hotmail.com>
2016-03-07 11:54:28 +0100

    small fix

--- src/ZKGPBTAI/bt/conditions/influence_map/HighTension.java
+++ src/ZKGPBTAI/bt/conditions/influence_map/HighTension.java
@@ -7,9 +7,10 @@ import bt.leaf.Condition;
  * Created by Jonatan on 24-Feb-16.
  */
 public class HighTension extends Condition<EconomyManager>{
-    EconomyManager em = getBlackboard();
+
     @Override
     protected boolean condition() {
+        EconomyManager em = getBlackboard();
         return em.influenceManager.im.isHighTension(em.getWorker(tree).getUnit().getPos());
     }
 }
