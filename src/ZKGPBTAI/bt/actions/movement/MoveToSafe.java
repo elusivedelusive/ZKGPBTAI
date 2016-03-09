a2be47f6aa4e4dec1d56e3ce2b3a506c30740701
Jonatan Hoff <jona341@hotmail.com>
2016-03-09 15:47:50 +0100

    updated movetoSafe

--- src/ZKGPBTAI/bt/actions/movement/MoveToSafe.java
+++ src/ZKGPBTAI/bt/actions/movement/MoveToSafe.java
@@ -12,13 +12,25 @@ import com.springrts.ai.oo.clb.Unit;
 /**
  * Created by Jonatan on 29-Feb-16.
  */
-public class MoveToSafe extends WorkerAction{
+public class MoveToSafe extends WorkerAction {
 
     @Override
     protected WorkerTask getWorkerTask() {
         EconomyManager em = getBlackboard().economyManager;
         final Worker worker = em.getWorker(tree);
 
+        if (em.caretakers.size() != 0) {
+            float closest = Float.MAX_VALUE;
+            AIFloat3 closestPos = em.caretakers.get(0).getPos();
+            for (Unit u : em.caretakers) {
+                float dist = Utility.distance(u.getPos(), worker.getPos());
+                if (dist < closest) {
+                    closest = dist;
+                    closestPos = u.getPos();
+                }
+            }
+            return em.createMoveTask(worker, closestPos);
+        }
         return em.createMoveTask(worker, Utility.getNearestSafeHaven(em.influenceManager.im, worker.getUnit().getPos()));
     }
 
